e7c00600974495ae82715ca95a2756fadb01c235
Snowapril <sinjihng@naver.com>
2021-03-13 18:18:52 +0900

    replace non-constant size array declaration to vector
    As non-constant size array declaration is non-standard, I replaced it to vector

--- Renderer/OpenXRUtil.cpp
+++ Renderer/OpenXRUtil.cpp
@@ -137,9 +137,9 @@ namespace Util {
                 return false;
             }
 
-            XrReferenceSpaceType referenceSpaces[referenceSpacesCount];
+            std::vector<XrReferenceSpaceType> referenceSpaces(referenceSpacesCount);
             if(XR_FAILED(xrEnumerateReferenceSpaces(session, referenceSpacesCount,
-                                                &referenceSpacesCount, referenceSpaces))) {
+                                                &referenceSpacesCount, referenceSpaces.data()))) {
                 fprintf(stderr, "Enumerating reference spaces failed!\n");
                 return false;
             }
@@ -182,13 +182,13 @@ namespace Util {
             XR_CHECK_RESULT(xrEnumerateInstanceExtensionProperties(
                     nullptr, 0, &instanceExtensionCount, nullptr))
 
-            XrExtensionProperties instanceExtensionProperties[instanceExtensionCount];
+            std::vector<XrExtensionProperties> instanceExtensionProperties(instanceExtensionCount);
             for(auto &&props : instanceExtensionProperties)
                 props.type = XR_TYPE_EXTENSION_PROPERTIES;
 
             XR_CHECK_RESULT(xrEnumerateInstanceExtensionProperties(nullptr, instanceExtensionCount,
                                                             &instanceExtensionCount,
-                                                            instanceExtensionProperties))
+                                                            instanceExtensionProperties.data()))
 
             for(auto props : instanceExtensionProperties) {
                 if(!strcmp(XR_KHR_VULKAN_ENABLE_EXTENSION_NAME, props.extensionName))
@@ -234,10 +234,10 @@ namespace Util {
                 return false;
             }
 
-            XrViewConfigurationType viewConfigurations[viewConfigurationCount];
+            std::vector<XrViewConfigurationType> viewConfigurations(viewConfigurationCount);
             if(XR_FAILED(xrEnumerateViewConfigurations(
                     instance, system_id, viewConfigurationCount,
-                    &viewConfigurationCount, viewConfigurations))) {
+                    &viewConfigurationCount, viewConfigurations.data()))) {
                 fprintf(stderr, "Failed to enumerate view configurations!\n");
                 return false;
             }
